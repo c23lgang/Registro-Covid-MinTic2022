d48b63c81862d71ed1a485a3bbf993181d8a43a2
c23lgang <contact@c23lgang.cl>
2021-11-12 11:55:37 -0500

    inicie mi registro de vacunacion

---
+++ js/validar.js
@@ -0,0 +1,12 @@
+//CREACION DE FUNCIONES EN JAVASCRIPT
+
+//FUNCION MOSTRAR PASSWORD
+function monstrarPassword(obj){
+    var obj = document.getElementById("password")
+    obj.type="text"
+}
+
+function ocultarPassword(obj){
+    var obj = document.getElementById("password")
+    obj.type="password"
+}
